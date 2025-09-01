79d4c5bbaf385b98e5c6df2ca63484c3b795de5e
Ulises Rabasa <rabasaulises@gmail.com>
2025-09-01 11:16:39 -0300

    Agrego programa principal de la calculadora

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ =="__main__":
+    num=float(input("Ingrese un numero"))
+    otro_numero=float(input("Ingrese otro numero"))
+    operacion=input("Ingrese una operacion(suma,resta,miltiplicacion,division)")
+    #usar calculadora
+    #imprimir resultado
+    
