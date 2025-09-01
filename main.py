771c86bdcb9d36696f2f71c26115eedebdf99cd3
JUAN IGNACIO COELHO SORIA <lapampa2003@gmail.com>
2025-09-01 13:05:35 -0300

    Modificacion del main en feature/programa-inicial

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 if __name__ =="__main__":
-    num=float(input("Ingrese un numero"))
-    otro_numero=float(input("Ingrese otro numero"))
-    operacion=input("Ingrese una operacion(suma,resta,miltiplicacion,division)")
+    num=float(input("Ingrese un número: "))
+    otro_numero=float(input("Ingrese otro número: "))
+    operacion=input("Ingrese una operacion(A: suma, B: resta, C: multiplicacion, D: division): ")
     #usar calculadora
     #imprimir resultado
     
