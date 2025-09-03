6858a95b589d3345c80d4935023ca2f055b91f8a
Juan Manuel Vila <512juanm@gmail.com>
2025-09-02 21:30:21 -0300

    feat: operacion de division agregada a la calculadora.

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,8 @@ def multiplicacion(num, otro_numero):
 
 def resta(a, b):
     return a - b
+
+def division(a, b):
+    if (b == 0):
+        return "Error en la division! No se puede dividir por 0."
+    return a / b
