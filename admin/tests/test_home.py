44e4db68b8a9c0e8c5495f1cb77ed90bad0f7bb2
Adrián A. Sambido <113736411+AdrianASambido@users.noreply.github.com>
2025-09-08 23:29:27 -0300

    Creamos el primer test

--- admin/tests/test_home.py
+++ admin/tests/test_home.py
@@ -8,4 +8,4 @@ client = app.test_client()
 def test_home():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
+    assert response.data == b'Hello, world!'
