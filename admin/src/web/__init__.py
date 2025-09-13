012641f5d4164548d49e8e06d0861699c87981e9
Juan Manuel Vila <512juanm@gmail.com>
2025-09-13 17:01:13 -0300

    fix: simplificando los layouts

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -6,19 +6,19 @@ def create_app(env="development", static_folder="../../static"): #../../static
 
     @app.route("/")
     def home():
-        return render_template("home.html", sidebar_selected="", request={"user": {"is_authenticated": False}}, perms={"publico": {"cliente": False, "empleado": False, "admin": False}   })
+        return render_template("home.html"), 200
     
     @app.errorhandler(401)
     def unauthorizedError(error):
-        return render_template('errores/401.html', request={"user": {"is_authenticated": False}}), 401
+        return render_template('errores/401.html'), 401
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template('errores/404.html', request={"user": {"is_authenticated": False}}), 404
+        return render_template('errores/404.html'), 404
     
     @app.errorhandler(500)
     def internalError(error):
-        return render_template('errores/500.html', request={"user": {"is_authenticated": False}}), 500
+        return render_template('errores/500.html'), 500
 
     @app.route("/error-401")
     def throw_401_error_for_test():
