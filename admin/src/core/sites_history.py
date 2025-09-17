f744354a970d524e63512056871085e4c3583ab2
Juan Manuel Vila <512juanm@gmail.com>
2025-09-17 13:28:32 -0300

    feat: mejorando la interfaz y los datos de ejemplo

--- admin/src/core/sites_history.py
+++ admin/src/core/sites_history.py
@@ -10,14 +10,33 @@ site_1_history= [
         "sitio_id": 1,
         "usuario_modificador_id": 1,
         "fecha_modificacion": datetime.datetime.now(),
+        "accion": "crear",  # crear, editar, eliminar, cambiar_tags, cambiar_imagenes
+        "cambios": [
+            {
+                "campo": "nombre",
+                "valor_anterior": None,
+                "valor_nuevo": "Chicho itza"
+            },
+            {
+                "campo": "descripcion",
+                "valor_anterior": None,
+                "valor_nuevo": "Sitio ubicado en........"
+            }
+            # ... más cambios
+        ]
+    },
+    {
+        "id": 2,
+        "sitio_id": 1,
+        "usuario_modificador_id": 2,
+        "fecha_modificacion": datetime.datetime.now(),
         "accion": "editar",  # crear, editar, eliminar, cambiar_tags, cambiar_imagenes
         "cambios": [
             {
-                "campo": "nombre_del_campo",
-                "valor_anterior": "valor anterior",
-                "valor_nuevo": "valor nuevo"
+                "campo": "nombre",
+                "valor_anterior": "Chicho itza",
+                "valor_nuevo": "Chichen Itza"
             }
-            # ... más cambios
         ]
     }
 ]
@@ -33,9 +52,13 @@ usuario_2={
     "nombre": "Usuario 2",
     "apellido": "Apellido 2",
 }
-sitio={
+sitio_1={
     "id": 1,
     "nombre": "Chichen Itza",
+}
+sitio_2={
+    "id": 2,
+    "nombre": "Machu Picchu",
 }
 
 def list_site_history(sitio_id):
@@ -43,7 +66,10 @@ def list_site_history(sitio_id):
     Retorna una lista de todos los sitios historicos.
     """
     if (sitio_id != 1):
-        return None
+        return {
+        "sitio": sitio_2,
+        "historial": []
+    }
     
     for cambios in site_1_history:
         if(cambios["usuario_modificador_id"] == 1):
@@ -52,6 +78,6 @@ def list_site_history(sitio_id):
             cambios["datos_usuario"] = usuario_2
 
     return {
-        "sitio": sitio,
+        "sitio": sitio_1,
         "historial": site_1_history
     }
