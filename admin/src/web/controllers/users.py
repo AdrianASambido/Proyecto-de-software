4d386642b2db571ecd8675431e9a81775150c5fe
JUAN IGNACIO COELHO SORIA <lapampa2003@gmail.com>
2025-09-21 16:49:00 -0300

    Base creada para el modulo de agregar usuarios

--- admin/src/web/controllers/users.py
+++ admin/src/web/controllers/users.py
@@ -4,9 +4,21 @@
 
 from flask import Blueprint
 from flask import render_template, request, redirect, url_for
+from src.core import board_usuarios
 
 bp = Blueprint("users", __name__, url_prefix=("/usuarios"))
 
 @bp.get("/")
 def index():
-    return render_template("usuarios/tabla_usuarios.html"), 200
+    usuarios = board_usuarios.list_users()
+    return render_template("usuarios/tabla_usuarios.html", users=usuarios), 200
+
+@bp.get("/nuevo_usuario")
+def add_form():
+    return render_template("usuarios/agregar_usuario.html"), 200
+
+@bp.post("/agregar_usuario")
+def add_user():
+    user_data = dict(request.form)
+    board_usuarios.add_user(user_data)
+    return redirect(url_for("users.index"))
