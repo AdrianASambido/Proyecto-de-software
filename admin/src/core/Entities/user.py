389186fe388e18b5b61791bae4825ffc3006855e
AdrianSambido <adriinform2021@gmail.com>
2025-09-26 18:55:07 -0300

    corrección

--- admin/src/core/Entities/user.py
+++ admin/src/core/Entities/user.py
@@ -1,20 +1,3 @@
-from src.core.database import db
-from datetime import datetime, timezone
-
-class User(db.Model):
-    """
-    Modelo que representa un usuario en la base de datos
-    """
-    
-    __tablename__ = "users"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    
-    def __repr__(self):
-        return f"<User {self.username}>"
 import enum
 from src.core.database import db
 from sqlalchemy import Enum
