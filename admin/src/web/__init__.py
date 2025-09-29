a9b0bfda631d7701af76f28b3551f9d0afedfa06
Ulises Rabasa <rabasaulises@gmail.com>
2025-09-29 15:02:41 -0300

    fix:importo bcrypt

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -15,7 +15,7 @@ from src.web.controllers.sites_history import bp as sites_history_bp
 from src.web.controllers.sites import bp as sites_bp
 from src.web.controllers.tags import bp as tags_bp
 from src.web.controllers.feature_flags import bp as feature_flags_bp
-
+from src.web.controllers.login import bp as login_bp
 
 def pre_request_logging():
     # Revisar si está logueado
@@ -27,7 +27,7 @@ def create_app(env="development", static_folder="../../static"):  # ../../static
     app.config.from_object(config[env])
 
     database.init_app(app)
-    sesion.init_app(app)
+   
 
     app.before_request_funcs = {"users_bp": [pre_request_logging]}
 
