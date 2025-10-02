605b4e786e2772c0a1e0f31b4183967839ac894f
Juan Manuel Vila <512juanm@gmail.com>
2025-10-02 17:21:56 -0300

    fix: unauthorizedError le faltaba renderizar el template

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -77,6 +77,7 @@ def create_app(env="development", static_folder="../../static"):  # ../../static
 
     @app.errorhandler(401)
     def unauthorizedError(error):
+        return render_template("errores/401.html"), 401
 
     @app.errorhandler(404)
     def page_not_found(error):
