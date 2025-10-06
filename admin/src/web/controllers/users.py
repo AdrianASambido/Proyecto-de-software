c11538bca51b3e6b857e187c49152cf8e659731b
JUAN IGNACIO COELHO SORIA <lapampa2003@gmail.com>
2025-10-06 15:06:06 -0300

    Enviando mis datos de este modulo a dev

--- admin/src/web/controllers/users.py
+++ admin/src/web/controllers/users.py
@@ -5,8 +5,8 @@
 from flask import Blueprint
 from flask import render_template, request, redirect, url_for, flash
 from src.core.services import users as board_users
-from src.core.formularios import RegistrationForm, EditUserForm
-from src.core.auth import login_required
+from src.core.formularios import RegistrationForm, EditUserForm, EditUserAdminForm
+from src.core.auth import login_required, permission_required
 bp = Blueprint("users", __name__, url_prefix=("/usuarios"))
 
 
@@ -14,6 +14,7 @@ bp = Blueprint("users", __name__, url_prefix=("/usuarios"))
 
 @bp.get("/lista_usuarios")
 @login_required
+@permission_required("user_index")
 def index():
     """Muestra la lista de usuarios.
     Renderiza la plantilla con la lista de usuarios.
@@ -34,6 +35,7 @@ def index():
 
 @bp.route("/agregar_usuario", methods=["GET", "POST"])
 @login_required
+@permission_required("user_create")
 def add_user():
     form = RegistrationForm()
 
@@ -49,6 +51,8 @@ def add_user():
         board_users.add_user(user_data)
         flash("Usuario agregado exitosamente.", "success")
         return redirect(url_for("users.index"))
+    else:
+        print(form.errors)
     
     return render_template("usuarios/agregar_usuario.html", form=form), 200
 
@@ -56,12 +60,43 @@ def add_user():
 @login_required
 def edit_user(user_id):
     usuario = board_users.get_user_by_id(user_id)
-    form = EditUserForm(obj=usuario)
+    form = EditUserForm(obj=usuario, original_email=usuario.email)
     if form.validate_on_submit():
         nuevos_datos = form.data
         print("Se consiguieron los datos nuevos")
         board_users.update_user(user_id, nuevos_datos)
         print("Se guardaron los datos nuevos")
         return redirect(url_for("users.index"))
+    else:
+        print(form.errors)
     
     return render_template("usuarios/editar_usuario.html", user=usuario, form=form), 200
+
+@bp.post("/eliminar_usuario/<int:user_id>")
+@login_required
+@permission_required("user_destroy")
+def delete_user(user_id):
+
+    if not board_users.get_user_by_id(user_id):
+        flash("Usuario no encontrado.", "error")
+
+    board_users.delete_user(user_id)
+    flash("Usuario eliminado exitosamente.", "success")
+    return redirect(url_for("users.index"))
+
+@bp.route("editar_usuario_admin/<int:user_id>", methods=["GET", "POST"])
+@login_required
+@permission_required("user_update")
+def edit_user_admin(user_id):
+    usuario = board_users.get_user_by_id(user_id)
+    form = EditUserAdminForm(obj=usuario, original_email=usuario.email)
+    if form.validate_on_submit():
+        nuevos_datos = form.data
+        print("Se consiguieron los datos nuevos")
+        board_users.update_user(user_id, nuevos_datos)
+        print("Se guardaron los datos nuevos")
+        return redirect(url_for("users.index"))
+    else:
+        print(form.errors)
+    
+    return render_template("usuarios/editar_usuario_admin.html", user=usuario, form=form), 200
