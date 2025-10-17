253451fe20b84b60a08b707b68e223061e24cfc8
Alexis Rodriguez <alexisrodriguezarteaga@gmail.com>
2025-10-17 10:49:53 -0300

    Instalar tailwind para los estilos

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -1,11 +1,13 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">¡Hola Tailwind + Vue!</h1>
+    <button class="mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">
+      Click acá
+    </button>
+  </div>
 </template>
+
 
 <style scoped></style>
