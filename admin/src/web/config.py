52cb79ed75afe2ac9676deef8d7cc67565aa2e38
JUAN IGNACIO COELHO SORIA <lapampa2003@gmail.com>
2025-11-06 10:58:54 -0300

    Ya empieza a funcionar el login de google

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -80,7 +80,7 @@ class DevelopmentConfig(Config):
     )
 
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_SECRET_ID")
 
 
 class TestingConfig(Config):
